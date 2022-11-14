142674fc7bae00ddce1e89116a088c121e481994
Bhavesh Patidar <68957019+Bhavesh121@users.noreply.github.com>
2022-11-14 06:40:13 +0530

    Add files via upload

---
+++ Assignment-12 28 Oct/Time Complexity.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,n;
+    for (i = n; i > 1; i = i / 2) {
+        cout << "letsupgrade";
+   //Time complexity of this program is 
+        // n/2^k > 1 
+        // n>2^k
+         
+        // k<logn
+    }
+}
+
+
